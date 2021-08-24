5870e93777c79280aba589b7e0f8ed0f9e39c7bd
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-08-24 06:17:42 -0400

    errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,7 @@ const initialForm = {
 const Login = () => {
 
     const [ formValues, setFormValues ] = useState(initialForm)
+    const [ errors, setErrors ] = useState()
 
     const { push } = useHistory();
 
@@ -26,10 +27,10 @@ const Login = () => {
             .then(res => {
                 localStorage.setItem('token', res.data.token)
                 setFormValues(initialForm)
-                push('/myplants')
+                push(`/myplants/${res.data.user_id}`)
                 document.location.reload();
             })
-            .catch(err => console.log(err))
+            .catch(err => setErrors(err.response.data.message))
     }
 
     return(
@@ -38,8 +39,9 @@ const Login = () => {
 
             {/* <h2>Welcome to WaterMyPlants!</h2>
             <h4>Please login to view your plants.</h4> */}
-
+            
             <form onSubmit={handleSubmit}>
+                {errors ? <p className="error">{errors}</p> : undefined}
                 <label htmlFor="username">Username:</label>
                 <input
                     type="text"
